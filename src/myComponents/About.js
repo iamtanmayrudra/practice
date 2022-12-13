241e1ef0b4daa057766fa515bbd3f962fb78e7df
tanmayrudra28091992 <69500423+tanmayrudra28091992@users.noreply.github.com>
2022-12-13 19:55:02 +0530

    practice app creation

---
+++ src/myComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Change Color</button>
+    </div>
+  )
+}
+
+export default About
