89a2a16a60780de9c8facc321cb70611d514f516
tanmayrudra <tanmayrudra555@gmail.com>
2023-02-10 03:18:53 +0530

    practice

--- src/myComponents/Form.js
+++ src/myComponents/Form.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
 
+function Form(props) {
 
+  const [text, setText] = useState("");
 
-function form(props) {
+  const changeColor = ()=>{
+    console.log("color changes")
+  }
+
+  const colorOncehange = (event)=>{
+    // console.log("color changes")
+    setText(event.target.value)
+  }
+
   return (
     <div>
       <h2>{props.heading}</h2>
-      <textarea name="" id="" cols="30" rows="10" placeholder="Fill the" style></textarea>
+      <textarea name="" id="" cols="30" rows="10" value={text} onchange={colorOncehange}></textarea>
+      <button onClick={changeColor}>Change Color</button>
     </div>
   )
 }
 
-export default form
+export default Form
