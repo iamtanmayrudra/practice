82658ea9f75c00a25df95cec6e87bbbd570a3b84
tanmayrudra <tanmayrudra555@gmail.com>
2023-03-10 04:04:09 +0530

    Practices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-
-import Hooks from "./myComponents/PracticeHooks"
-=======
->>>>>>> 91fdc5449d457f020cdb6411ef74550bd6151542
 
 import TextEnhance from "./myComponents/TextEnhance";
 import EnableMode from "./myComponents/EnableMode";
@@ -11,17 +6,11 @@ import EnableMode from "./myComponents/EnableMode";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <Hooks/>
-    </div>
-  )
-}
-=======
-    <TextEnhance />,
-    <EnableMode />
+    <>
+      <TextEnhance />
+      <EnableMode />
+    </>
   );
 }
 
 export default App
->>>>>>> 91fdc5449d457f020cdb6411ef74550bd6151542
