8eb2e137baf7a53eb9e843e4143ae071e062360d
tanmayrudra <tanmayrudra555@gmail.com>
2023-03-13 21:58:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import TextEnhance from "./myComponents/TextEnhance";
 import EnableMode from "./myComponents/EnableMode";
 
 
+
 function App() {
   return (
     <>
+      <header mode={mode} toggleMode={toggleMode}>
+        <label htmlFor="">
+          <input type="checkbox" /> Enable Dark Mode
+        </label>
+      </header>
       <TextEnhance />
       <EnableMode />
     </>
--- src/myComponents/TextEnhance.js
+++ src/myComponents/TextEnhance.js
@@ -5,22 +5,30 @@ function Hooks() {
   const [add, setAdd] = useState("Default Value");
 
   const handleUpClick = () => {
-    console.log("Clicked");
+    // console.log("Clicked");
     let upText = add.toLocaleUpperCase();
     setAdd(upText);
   };
 
   const handleLoClick = () => {
-    console.log("Clicked");
+    // console.log("Clicked");
     let upText = add.toLocaleLowerCase();
     setAdd(upText);
   };
 
-  const handleCear = () => {
-    console.log("Clicked");
+  const handleClear = () => {
+    // console.log("Clicked");
     let upText = "";
     setAdd(upText);
   };
+
+  const [btnSelect, setbtnSelect] = useState("Select All")
+  const handleSelect = () =>{
+    let textSelect = document.getElementById("textBox")
+    textSelect.select()
+    navigator.clipboard.writeText(textSelect.value)
+    setbtnSelect("All Selected")
+  }
   
   const handleOnChange = (event) => {
     console.log("Clicked");
@@ -29,16 +37,14 @@ function Hooks() {
 
   return (
     <div className="hooksArea" style={{ padding: "15px" }}>
-      <p> All the very {add}</p>
-      <textarea
-        value={add}
-        onChange={handleOnChange}
-        style={{ width: "100%", height: "100px" }}
-      />
+      <p> All the very</p>
+      <textarea value={add} onChange={handleOnChange} id="textBox" style={{ width: "100%", height: "100px" }} />
       <br></br>
       <button onClick={handleUpClick}>To Uppercase</button>
       <button onClick={handleLoClick}>To Lowercase</button>
-      <button onClick={handleCear}>Clear</button>
+      <button onClick={handleClear}>Clear</button>
+      <button onClick={handleSelect}>{btnSelect}</button>
+
 
       <p>
         {add.length} of charcter and {add}
